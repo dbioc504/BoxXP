fa3fb8f6dcf57d86fe15c284d1fd94a805fc39c4
Dominick Bioc <dbioc917@gmail.com>
2025-07-08 22:17:51 -0500

    replace login screen with popup

--- js/indexAuth.js
+++ js/indexAuth.js
@@ -1,5 +1,5 @@
 // js/indexAuth.js
-import { auth, logOut, onAuth } from './firebaseAuth.js?v=20250708';
+import { auth, logOut, onAuth } from './firebaseAuth.js?v=20250708-1';
 
 document.addEventListener('DOMContentLoaded', () => {
   const authLink   = document.getElementById('auth-link');
