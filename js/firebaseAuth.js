ff77b77de7f56da17a2e86a4b1bfb75c7f1721ce
dbioc504 <108502034+dbioc504@users.noreply.github.com>
2025-07-08 21:00:42 -0500

    Switch login flow to Firebase

--- js/firebaseAuth.js
+++ js/firebaseAuth.js
@@ -1,18 +1,30 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword,
-         createUserWithEmailAndPassword, signOut } from "firebase/auth";
+         createUserWithEmailAndPassword, signOut,
+         onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDHcPWT5CguvvKwg95-D4ROaMypGu5H4OA",
+  authDomain: "boxxp-f4f64.firebaseapp.com",
+  projectId: "boxxp-f4f64",
+  storageBucket: "boxxp-f4f64.appspot.com",
+  messagingSenderId: "274613580349",
+  appId: "1:274613580349:web:44b844b91c8a3eb271f054",
+  measurementId: "G-N8MJ2DD4QB"
+};
 
-export const auth = getAuth();
+const app  = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
-export async function signIn(email, password) {
-    return signInWithEmailAndPassword(auth, email, password);
+export function signIn(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
 }
 
-export async function createAccount(email, password) {
-    return createUserWithEmailAndPassword(auth, email, password);
+export function createAccount(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
 }
 
-export function logOut() {
-    return signOut(auth);
+export function logOut() {  return signOut(auth);
 }
+
+export { onAuthStateChanged };
